d6198423d215d66c50a4734905da5f6a71a8d90b
Christine-horrocks <splogy2001@hotmail.com>
2018-06-14 11:30:48 +0100

    JSDoc added and t.js file documented

--- dust/t.js
+++ dust/t.js
@@ -5,7 +5,10 @@ var i18n = require('i18n');
 i18n.configure(require('../config').i18n);
 
 module.exports = initHelper;
-
+/**
+* This helper allows dust to work with the i18n internationalization package.
+* It passes in parameters which are accessable through the params object, the dust helper then takes in those parameters as its own.
+*/
 function initHelper(dust, renderer, config) {
 	dust.helpers.t = function (chunk, ctx, bodies, params) {
 		var key = dust.helpers.tap(params.key, chunk, ctx)
