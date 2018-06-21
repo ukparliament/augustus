57ba39dd7f4391bb2f007dc9b65333832b499ec3
Christine-horrocks <splogy2001@hotmail.com>
2018-06-21 14:57:52 +0100

    Span and hint refactored and span removed from banner

--- test/unit/components/span.spec.js
+++
@@ -1,9 +0,0 @@
-const testHelper = require('../../helpers/test-helper');
-
-describe('Span dust component', function() {
-  testHelper.setupBefore()
-
-  it('should return html from the dust component', function(done) {
-    testHelper.shunterTest('span', 'components__span', 'components', done)
-  });
-});
--- test/unit/components/status/banner.spec.js
+++ test/unit/components/status/banner.spec.js
@@ -3,12 +3,8 @@ const testHelper = require('../../../helpers/test-helper');
 describe('Status Banner dust component', function() {
   testHelper.setupBefore()
 
-  it('when given a span, should return html from the dust component', function(done) {
-    testHelper.shunterTest('banner-span', 'components__status__banner', 'components/status', done)
-  });
-
-  it('when not given span, should return html from the dust component', function(done) {
-    testHelper.shunterTest('banner-no-span', 'components__status__banner', 'components/status', done)
+  it('should return html from the dust component', function(done) {
+    testHelper.shunterTest('banner', 'components__status__banner', 'components/status', done)
   });
 
 });
