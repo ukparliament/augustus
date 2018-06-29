9433fed50fca39dfc32cef5a9cd82e99489fcd6b
Christine-horrocks <splogy2001@hotmail.com>
2018-06-29 13:46:56 +0100

    Tests passing, card/seacrh/hint.spec.js removed

--- test/unit/components/card/search/hint.spec.js
+++
@@ -1,9 +0,0 @@
-const testHelper = require('../../../../helpers/test-helper');
-
-describe('Hint dust component', function() {
-  testHelper.setupBefore()
-
-  it('should return html from the dust component', function(done) {
-    testHelper.shunterTest('hint', 'components__card__search__hint', 'components/card/search', done)
-  });
-});
