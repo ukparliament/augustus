4b6f08c29c5d342e7e4d99777b4265ede010c02a
Christine-horrocks <splogy2001@hotmail.com>
2018-07-26 15:23:27 +0100

    Editorconfig added,  eslint updated and docker-compose working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 5
-    },
-    "rules": {
-        "indent": [
-            "error",
-            ""
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-unused-vars": [
+      "warn"
+    ],
+    "no-undef": [
+      "warn"
+    ],
+    "no-console": [
+      "warn"
+    ]
+  },
+  "globals": {
+    "UK_Parliament": true,
+    "appInsights": true,
+    "L": true,
+    "require": true,
+    "describe": true,
+    "process": true,
+  }
 };
