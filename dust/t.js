5847c5943b1062d0783c76a99b10f3cf005b7ab2
Christine-horrocks <splogy2001@hotmail.com>
2018-07-26 17:07:15 +0100

    JS linter clean up

--- dust/t.js
+++ dust/t.js
@@ -9,13 +9,13 @@ module.exports = initHelper;
 * This helper allows dust to work with the i18n internationalization package.
 * It passes in parameters which are accessable through the params object, the dust helper then takes in those parameters as its own.
 */
-function initHelper(dust, renderer, config) {
-	dust.helpers.t = function (chunk, ctx, bodies, params) {
-		let key = dust.helpers.tap(params.key, chunk, ctx)
+function initHelper(dust, renderer) {
+  dust.helpers.t = function (chunk, ctx, bodies, params) {
+    let key = dust.helpers.tap(params.key, chunk, ctx);
 
-		let data = dust.helpers.tap(params.data, chunk, ctx)
+    let data = dust.helpers.tap(params.data, chunk, ctx);
 		data = data ? data : {};
 
-		return chunk.write(i18n.__(key, data))
-	};
+		return chunk.write(i18n.__(key, data));
+};
 }
