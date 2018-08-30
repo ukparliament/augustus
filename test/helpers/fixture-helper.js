44f372f8103b6b398a7ff25e5396ad6caba45c42
Matt Rayner <m@rayner.io>
2018-08-30 14:16:55 +0100

    Re-structure and update integration tests
    * Changed integration tests to use the root data folder instead of duplicated fixtures
    * Updated tests

--- test/helpers/fixture-helper.js
+++ test/helpers/fixture-helper.js
@@ -4,24 +4,54 @@ const callsites = require('callsites');
 * This function takes in the path of the test file, the extension of the file type and the fixture location.
 * It then replaces either the helper part of the file path with fixtures/extension and fixture location.
 */
-function fixturePath(path, extension, fixtureLocation) {
-  var split_path = path.split('/');
+function fixturePath(path, extension, integrationTest) {
+  let split_path = path.split('/');
   split_path.splice(-1, 1);
-    var index = split_path.indexOf('helpers');
-    split_path[index] = 'fixtures/' + extension + '/' + fixtureLocation;
-  return split_path.join('/');
+
+  // Are we looking for fixtures in the PROJECT_ROOT/test/fixtures, or PROJECT_ROOT/data folders.
+  let folderMarker = integrationTest ? 'test' : 'helpers';
+  let fixtureFolder = integrationTest ? 'data' : 'fixtures';
+
+  let index = split_path.indexOf(folderMarker);
+
+  let new_path = split_path.slice(0, index);
+  new_path.push(fixtureFolder);
+
+  if(!integrationTest) {
+    new_path.push(extension);
+  }
+
+  return new_path.join('/');
 }
+
 /**
 * This function takes the file path from the test file the extension of the desired file type and the fixture location, and then passes it into the htmlFixturePath method along with the file name parameter and an html extention.
 * It returns the path to the fixture file which can then be used in the expects statement of the test.
+*
+* @param fileName [string] The fixture file name i.e. '404.json' would have filename '404'
+* @param extension [string] The fixture extension we are looking for i.e. '404.json' would have extension 'json'
+* @param fixtureLocation [string|null] The directory within our fixtures folder to look for our file i.e. 'test/fixtures/integration/html/error-pages/4xx/404.html' would have the fixtureLocation 'error-pages/4xx'
+* @param integrationTest [boolean] Are we getting an integration test fixture. Used specifically to get JSON fixtures from the root data directory.
+*
+* @return [string] the fixture file we will be using
 */
 module.exports = {
-  getFixture: function(file_name, extension, fixtureLocation){
-    var file_path = fixturePath(callsites()[1].getFileName(), extension, fixtureLocation) + '/' + file_name + '.' + extension;
-    if (extension == 'json') {
-      return JSON.parse(fs.readFileSync(file_path, 'utf8'));
-    } else {
-      return fs.readFileSync(file_path, 'utf8');
+  getFixture: function(fileName, extension, fixtureLocation, integrationTest){
+    // What directory are we looking in?
+    let filePath = fixturePath(callsites()[1].getFileName(), extension, integrationTest);
+
+    // Build our file path
+    if(fixtureLocation){
+      filePath += `/${fixtureLocation}`;
     }
+    filePath += `/${fileName}.${extension}`;
+
+    // Read the file
+    let fixture = fs.readFileSync(filePath, 'utf8');
+    if (extension === 'json') {
+      fixture = JSON.parse(fixture);
+    }
+
+    return fixture;
   }
-}
+};
--- test/integration/error-pages/404.spec.js
+++ test/integration/error-pages/404.spec.js
@@ -4,6 +4,6 @@ describe('Error pages', () => {
   testHelper.setupBefore();
 
   it('should render the 404 page', (done) => {
-    testHelper.shunterTest('404', 'layout-error-404', 'integration/error-pages', done);
+    testHelper.shunterTest('404', 'layout-error-404', 'error-pages', done, true);
   })
-})
+});
