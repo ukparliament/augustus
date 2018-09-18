9c446de2d7cad8d3e1f5f8ade2d8b10e594b62a6
Allan Wazacz <cazwazacz@gmail.com>
2018-09-18 13:45:50 +0100

    [WEBDEV-698] Log to disk in production - Fixes the issue where in production was only logging in JSON to stdout - Now it logs both as JSON to stdout and to the disk in log/production.log and log/error.log

--- middlewares.js
+++ middlewares.js
@@ -6,12 +6,22 @@ const healthCheck = require('./middlewares/healthCheck');
 const morgan = require('./middlewares/morgan');
 const setCloudflareID = require('./middlewares/setCloudflareID');
 
+const winstonErrorTransport = require('./logging/transports/winston-error');
+const winstonProductionTransport = require('./logging/transports/winston-production');
+const winstonCloudflareIDFilter = require('./logging/filters/cloudflareID');
+
 let bootstrap = (app) => {
   // If NODE_ENV is production or PRODUCTION_LOGGING is a true string, the default Shunter logging is silenced and JSON logging is used in its place
   if (process.env.NODE_ENV === 'production'|| process.env.PRODUCTION_LOGGING === 'true') {
     app.use(morgan);
 
-    app.getConfig().log = new winston.Logger({ silent: true });
+    app.getConfig().log = new winston.Logger({
+      transports: [
+        winstonErrorTransport(),
+        winstonProductionTransport(),
+      ],
+      filters: [winstonCloudflareIDFilter]
+    });
   }
 
   app.use(setCloudflareID);
