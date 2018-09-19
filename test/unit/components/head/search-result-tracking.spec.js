2e17b486735de0e7f55a540604b84935254b5bda
Rebecca Appleyard <rklappleyard@gmail.com>
2018-09-19 16:32:40 +0100

    [WEBDEV-703] Addition of createFixture function * This will enable html fixtures to be generated for testing.

--- test/unit/components/head/search-result-tracking.spec.js
+++ test/unit/components/head/search-result-tracking.spec.js
@@ -4,6 +4,6 @@ describe('Search result specific appInsights script dust component', function() 
   testHelper.setupBefore()
 
   it('should return html from the dust component', function(done) {
-    testHelper.shunterTest('search-result-tracking', 'components__head__search-result-tracking', 'components/head', done)
+    testHelper.shunterTest('search-result-tracking', 'components__head__search-result-tracking', 'components/head', done);
   });
 });
