59d66b2ddc619ee785515182945430c1509eb2c6
Allan Wazacz <cazwazacz@gmail.com>
2018-10-19 10:17:00 +0100

    [WEBDEV-849] Change static data path for group layings Previously: /groups/:group/layings Now: /groups/:group/made-available/availability-types/layings

--- test/integration/groups/12345678/layings/index_page.spec.js
+++
@@ -1,9 +0,0 @@
-const testHelper = require('../../../../helpers/test-helper');
-
-describe('Group layings index page', function(){
-  testHelper.setupBefore();
-
-  it('should render content for the group layings index page', function(done){
-    testHelper.shunterTest('index', 'layout', 'groups/12345678/layings', done, true);
-  });
-});
