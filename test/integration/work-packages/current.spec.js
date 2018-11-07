b895dbed9687a87a9397c7144265488888a63a7f
Rebecca Appleyard <rklappleyard@gmail.com>
2018-11-07 15:13:27 +0000

    Updated JSON and tests for work packages and paper types

---
+++ test/integration/work-packages/current.spec.js
@@ -0,0 +1,9 @@
+const testHelper = require('../../helpers/test-helper');
+
+describe('Work Packages current page', function(){
+  testHelper.setupBefore();
+
+  it('should render content for work packages current page', function(done){
+    testHelper.shunterTest('current', 'layout', 'work-packages', done, true);
+  });
+});
